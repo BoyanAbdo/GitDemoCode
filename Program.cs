a2422862f8f7a204c22482ef2ddc25dd77f2fb46
Boyan Abdo <boyan.abdo@gmail.com>
2020-09-15 22:42:00 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitTrialCodeSecond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Git! Second");
+        }
+    }
+}
